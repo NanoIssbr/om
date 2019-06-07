66167f5ebaf1fafa553558cf2db1c88304d7266e
ibrayche <ibrayche@ibrayche-Latitude-E5540>
2019-06-07 12:22:52 +0100

    commit 4

--- officeManagement/src/main/java/com/om/services/serviceImpl/AppointmentServiceImpl.java
+++ officeManagement/src/main/java/com/om/services/serviceImpl/AppointmentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.om.services.serviceImpl;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,5 +33,22 @@ public class AppointmentServiceImpl implements AppointmentService {
 		Appointment appointment = findAppointment(id);
 		appointment.setConfirmed(true);
 		appointmentDao.save(appointment);
+	}
+	
+	public List<Appointment> getFuturAppointments(){
+		List<Appointment> futurAppointments = new ArrayList<>();
+		List<Appointment> allAppointments = findAll();
+		Date sysDate = new Date();
+		if(allAppointments != null && !allAppointments.isEmpty() ) {
+			for(Appointment apnt : allAppointments) {
+				if(apnt.getDate() == null) {
+					continue;
+				}
+				if(apnt.getDate().compareTo(sysDate) >= 0) {
+					futurAppointments.add(apnt);
+				}
+			}
+		}
+		return futurAppointments;
 	}
 }
